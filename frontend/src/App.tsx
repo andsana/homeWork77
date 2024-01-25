161e73b977d7959a06e3d250b2a1c075c1ad4f78
Susanna Andreeva <sanadex90@gmail.com>
2024-01-26 00:47:30 +0900

    add MessageForm.tsx, FileInput.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import Layout from './components/Layout/Layout';
+import { Routes } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div>test</div>
-  )
+    <Layout>
+      <Routes>
+
+      </Routes>
+
+    </Layout>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/MessageForm/MessageForm.tsx
@@ -0,0 +1,91 @@
+import { MessageMutation } from '../../types';
+import React, { useState } from 'react';
+import FileInput from '../FileInput/FileInput';
+import { Grid, TextField } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import SaveIcon from '@mui/icons-material/Save';
+
+const initialState: MessageMutation = {
+  author: '',
+  message: '',
+  image: null,
+};
+
+interface Props {
+  onSubmit: (message: MessageMutation) => void;
+  isLoading?: boolean;
+}
+
+const MessageForm: React.FC<Props> = ({onSubmit, isLoading = false}) => {
+  const [message, setMessage] = useState(initialState);
+
+  const changeMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const fileInputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, files} = e.target;
+    if (files) {
+      setMessage(prevState => ({
+        ...prevState,
+        [name]: files[0],
+      }));
+    }
+  };
+
+  const onFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (isLoading) return;
+    onSubmit(message);
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+      <Grid container direction="column">
+        <Grid item xs>
+          <TextField
+            required
+            id="author" label="Author"
+            name="author"
+            value={message.author}
+            onChange={changeMessage}
+          />
+        </Grid>
+        <Grid item xs>
+          <TextField
+            required
+            id="message" label="Message"
+            name="message"
+            value={message.message}
+            onChange={changeMessage}
+          />
+        </Grid>
+        <Grid item xs>
+          <FileInput
+            onChange={fileInputChangeHandler}
+            name="image"
+            label="Product image"
+          />
+        </Grid>
+        <Grid item xs>
+          <LoadingButton
+            type="submit"
+            color="primary"
+            variant="contained"
+            disabled={isLoading}
+            loading={isLoading}
+            loadingPosition="start"
+            startIcon={<SaveIcon/>}
+          >
+            Create
+          </LoadingButton>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default MessageForm;
